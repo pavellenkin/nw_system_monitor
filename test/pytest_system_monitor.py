28ed87b4593f410467bce1b661dd39a006e6c062
pavellenkin <pavel-lenkin@yandex.ru>
2025-01-20 10:37:53 +0300

    the final version

--- test/pytest_system_monitor.py
+++ test/pytest_system_monitor.py
@@ -9,10 +9,10 @@ def test_page_loading():
 
 
 def test_valid():
-    assert is_valid(newval="12") is True
-    assert is_valid(newval="+1234") is False
-    assert is_valid(newval="999") is True
-    assert is_valid(newval="ABC") is False
+    assert is_valid("12") is True
+    assert is_valid("+1234") is False
+    assert is_valid("999") is True
+    assert is_valid("ABC") is False
 
 
 pytest.main()
